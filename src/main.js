6e6433ab3b0ae12049f058c9d1627371d1f713b7
Gagan Kahal <73100470+gaganjot99@users.noreply.github.com>
2022-12-09 11:31:14 +0530

    feat: add scroll effects and svg draw func

--- src/main.js
+++ src/main.js
@@ -1,3 +1,38 @@
 gsap.registerPlugin("ScrollTrigger");
 
-gsap.from("h1, .logo",{scrollTrigger: ".logo", duration: 2, scale: 1.5, x: 400})
+//gsap.set(".all-fill", { position: "absolute" });
+
+gsap.from(".welcome-text", {
+  scrollTrigger: {
+    trigger: ".img-div",
+    markers: true,
+    scrub: true,
+    pin: true,
+    start: "top top",
+    end: "+=100%",
+    pinSpacing: true,
+  },
+  duration: 2,
+  opacity: 0,
+  stagger: 0.5,
+});
+
+const ele = document.getElementsByClassName("cls-1")[0];
+const length = ele.getTotalLength();
+
+gsap.set(".cls-1", { strokeDasharray: "" + length + "," + length });
+
+gsap.from(".cls-1", {
+  scrollTrigger: {
+    trigger: ".Second",
+    markers: true,
+    scrub: 1.5,
+    pin: true,
+    start: "top top",
+    end: "+=100%",
+    pinSpacing: true,
+  },
+  duration: 2,
+  strokeDasharray: "" + 0 + ", " + length,
+  stagger: 0.5,
+});
